ba29471f5af053f21f2ed4e202f5c83b94cc6d25
Michael duPont <michael@mdupont.com>
2018-09-15 15:45:35 -0400

    Replace with new Django Project

---
+++ castpage/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Cast(models.Model):
+    name = models.CharField(max_length=200)
+    
+    
+    
+    # author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    # title = models.CharField(max_length=200)
+    # text = models.TextField()
+    # created_date = models.DateTimeField(
+    #         default=timezone.now)
+    # published_date = models.DateTimeField(
+    #         blank=True, null=True)
+
+    # def publish(self):
+    #     self.published_date = timezone.now()
+    #     self.save()
+
+    # def approved_comments(self):
+    #     return self.comments.filter(approved_comment=True)
+
+    # def __str__(self):
+    #     return self.title
