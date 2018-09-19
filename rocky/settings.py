c38cdf874813ae2940dce8960c242bb5ece4aca1
Michael duPont <michael@mdupont.com>
2018-09-18 21:36:53 -0400

    Add static folder

--- rocky/settings.py
+++ rocky/settings.py
@@ -127,8 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 LOGIN_URL = '/accounts/login/'
