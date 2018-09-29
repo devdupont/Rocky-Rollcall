8d510d271d2e7a5267fd4f48b1ff46640772e739
Michael duPont <michael@mdupont.com>
2018-09-29 16:47:03 -0400

    Add user signup

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -7,4 +7,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='landing_page'),
+    path('signup', views.signup, name='signup'),
 ]
--- landingpage/views.py
+++ landingpage/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +9,20 @@ def home(request):
     Renders the landing page
     """
     return render(request, 'landingpage/landingpage.html')
+
+def signup(request):
+    """
+    Create and validate a new user
+    """
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('landing_page')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
