34bd5c3dbd59a756ed3c4ef9ca9a82550a217133
Michael duPont <michael@mdupont.com>
2018-10-14 20:11:28 -0500

    Shorten slug urls

--- castadmin/urls.py
+++ castadmin/urls.py
@@ -5,12 +5,14 @@ Cast admin URL patterns
 from django.urls import path
 from . import views
 
+_s = '<slug:slug>/'
+
 urlpatterns = [
-    path('<slug:slug>/', views.cast_admin, name='cast_admin'),
-    path('<slug:slug>/section/new/', views.section_new, name='cast_section_new'),
-    path('<slug:slug>/section/<int:pk>/edit/', views.section_edit, name='cast_section_edit'),
-    path('<slug:slug>/section/<int:pk>/remove/', views.section_delete, name='cast_section_delete'),
-    path('<slug:slug>/edit', views.cast_edit, name='cast_edit'),
-    path('<slug:slug>/managers', views.managers_edit, name='cast_managers_edit'),
-    path('<slug:slug>/managers/delete/<int:pk>', views.managers_delete, name='cast_managers_delete'),
+    path(_s, views.cast_admin, name='cast_admin'),
+    path(_s+'section/new/', views.section_new, name='cast_section_new'),
+    path(_s+'section/<int:pk>/edit/', views.section_edit, name='cast_section_edit'),
+    path(_s+'section/<int:pk>/remove/', views.section_delete, name='cast_section_delete'),
+    path(_s+'edit', views.cast_edit, name='cast_edit'),
+    path(_s+'managers', views.managers_edit, name='cast_managers_edit'),
+    path(_s+'managers/delete/<int:pk>', views.managers_delete, name='cast_managers_delete'),
 ]
